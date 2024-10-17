3f6d622e242de2c12fe92308e820b3f89b34c35b
Huaiying Luo <hl2446@cornell.edu>
2024-10-17 12:27:32 -0400

    complete task 2.4, mean_red failed

--- minitorch/tensor_functions.py
+++ minitorch/tensor_functions.py
@@ -116,8 +116,8 @@ class Mul(Function):
     def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, Tensor]:
         (t1, t2) = ctx.saved_values
         return (
-            grad_output.f.mul_zip(t1, grad_output), 
-            grad_output.f.mul_zip(t2, grad_output)
+            grad_output.f.mul_zip(t2, grad_output), 
+            grad_output.f.mul_zip(t1, grad_output)
             )
 
 
